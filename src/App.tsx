0bbb3ad8dbec27ea84b8fbfc54ceef8df179c3e4
TonNQ <nguyenquoctoan12122003@gmail.com>
2024-07-16 16:05:36 +0700

    feat: config eslint and routing
    Config eslint and routing
    
    86epuuq8e

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import React, { useState, useEffect } from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import Loader from './common/Loader'
-import PageTitle from './components/PageTitle'
-import DefaultLayout from './layout/DefaultLayout'
-import Dashboard from './pages/Dashboard/Dashboard'
-import Ticket from './pages/Ticket'
-import User from './pages/User'
-import Voucher from './pages/Voucher'
-import Cinema from './components/Cinema/Cinema'
-import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import useRouteElements from './router/useRouteElements'
+import { ToastContainer } from 'react-toastify'
 function App() {
   const [loading, setLoading] = useState<boolean>(true)
   const { pathname } = useLocation()
+  const routeElements = useRouteElements()
 
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -27,57 +22,10 @@ function App() {
   return loading ? (
     <Loader />
   ) : (
-    <DefaultLayout>
+    <>
       <ToastContainer />
-      <Routes>
-        <Route
-          path='/dashboard'
-          element={
-            <>
-              <PageTitle title='Dashboard | Cinestar' />
-              <Dashboard />
-            </>
-          }
-        />
-        <Route
-          path='/cinema'
-          element={
-            <>
-              <PageTitle title='Cinema | CineStar' />
-              <Cinema />
-            </>
-          }
-        />
-
-        <Route
-          path='/ticket'
-          element={
-            <>
-              <PageTitle title='Ticket | CineStar' />
-              <Ticket />
-            </>
-          }
-        />
-        <Route
-          path='/user'
-          element={
-            <>
-              <PageTitle title='User | CineStar' />
-              <User />
-            </>
-          }
-        />
-        <Route
-          path='/voucher'
-          element={
-            <>
-              <PageTitle title='Voucher | CineStar' />
-              <Voucher />
-            </>
-          }
-        />
-      </Routes>
-    </DefaultLayout>
+      {routeElements}
+    </>
   )
 }
 
